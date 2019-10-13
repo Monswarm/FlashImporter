cb97a716ee51347131fb00cb5a31d82fc850504c
Monswarm <monswarm@gmail.com>
2019-10-13 19:47:54 +0200

    Initial commit.
    Initial commit. First release, pending to make a full tutorial in README file.

---
+++ Assets/Monswarm/Editor/MonswarmFlashImporter/JSONAnimation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monswarm.Editor.MonswarmFlashImporter
+{
+    public class JSONAnimation
+    {
+        [System.Serializable]
+        public class LayersInformation
+        {
+            public LayersInfoAnimation ANIMATION;
+            public LayersInfoMetadata metadata;
+        }
+
+        [System.Serializable]
+        public class LayersInfoMetadata
+        {
+            public float framerate;
+        }
+
+        [System.Serializable]
+        public class LayersInfoAnimation
+        {
+            public string SYMBOL_name;
+            public LayersInfoTimeline TIMELINE;
+        }
+
+        [System.Serializable]
+        public class LayersInfoTimeline
+        {
+            public LayersInfoLayers[] LAYERS;
+        }
+
+        [System.Serializable]
+        public class LayersInfoLayers
+        {
+            public string Layer_name;
+            public LayersInfoFrames[] Frames;
+        }
+
+        [System.Serializable]
+        public class LayersInfoFrames
+        {
+            public int index;
+            public int duration;
+            public LayersInfoElements[] elements;
+        }
+
+        [System.Serializable]
+        public class LayersInfoElements
+        {
+            public LayersInfoAtlasSpriteInstance ATLAS_SPRITE_instance;
+        }
+
+        [System.Serializable]
+        public class LayersInfoAtlasSpriteInstance
+        {
+            public string name;
+            public LayersInfoPosition Position;
+        }
+
+        [System.Serializable]
+        public class LayersInfoPosition
+        {
+            public int x;
+            public int y;
+        }
+    }
+}
---
+++ Assets/Monswarm/Editor/MonswarmFlashImporter/JSONAtlas.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monswarm.Editor.MonswarmFlashImporter
+{
+    public class JSONAtlas
+    {
+        [System.Serializable]
+        public class AtlasInformation
+        {
+            public Atlas ATLAS;
+            public Metadata meta;
+        }
+
+        [System.Serializable]
+        public class Atlas
+        {
+            public Sprites[] SPRITES;
+        }
+
+        [System.Serializable]
+        public class Sprites
+        {
+            public SpriteInformation SPRITE;
+        }
+
+        [System.Serializable]
+        public class SpriteInformation
+        {
+            public string name;
+            public int x;
+            public int y;
+            public int w;
+            public int h;
+            public bool rotated;
+        }
+
+        [System.Serializable]
+        public class Metadata
+        {
+            public float framerate;
+            public Size size;
+        }
+
+        [System.Serializable]
+        public class Size
+        {
+            public float w;
+            public float h;
+        }
+    }
+}
